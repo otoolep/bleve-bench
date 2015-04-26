f67a096dc2672c6241c2b34215616694942d1c67
Philip O'Toole <philip.otoole@yahoo.com>
2015-04-25 21:14:10 -0700

    Commit benchmarking program

---
+++ bleve_indexer.go
@@ -0,0 +1,116 @@
+package indexer
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"github.com/blevesearch/bleve"
+)
+
+// Indexer represents the indexing engine.
+type Indexer struct {
+	path    string // Path to bleve storage
+	batchSz int    // Indexing batch size
+
+	shards []bleve.Index    // Index shards i.e. bleve indexes
+	alias  bleve.IndexAlias // All bleve indexes as one reference, for search
+}
+
+// New returns a new indexer.
+func New(path string, nShards, batchSz int) *Indexer {
+	return &Indexer{
+		path:    path,
+		batchSz: batchSz,
+		shards:  make([]bleve.Index, 0, nShards),
+		alias:   bleve.NewIndexAlias(),
+	}
+}
+
+// Open opens the indexer, preparing it for indexing.
+func (i *Indexer) Open() error {
+	if err := os.MkdirAll(i.path, 0755); err != nil {
+		return fmt.Errorf("unable to create index directory %s", i.path)
+	}
+
+	for s := 0; s < cap(i.shards); s++ {
+		path := filepath.Join(i.path, strconv.Itoa(s))
+		b, err := bleve.New(path, mapping())
+		if err != nil {
+			return fmt.Errorf("index %d at %s: %s", s, path, err.Error())
+		}
+
+		i.shards = append(i.shards, b)
+		i.alias.Add(b)
+	}
+
+	return nil
+}
+
+// Index indexes the given docs, dividing the docs evenly across the shards.
+// Blocks until all documents have been indexed.
+func (i *Indexer) Index(docs [][]byte) error {
+	base := 0
+	docsPerShard := (len(docs) / len(i.shards))
+	var wg sync.WaitGroup
+
+	wg.Add(len(i.shards))
+	for _, s := range i.shards {
+		go func(b bleve.Index, ds [][]byte) {
+			defer wg.Done()
+
+			batch := b.NewBatch()
+			n := 0
+
+			// Just index whole batches.
+			for n = 0; n < len(ds)-(len(ds)%i.batchSz); n++ {
+				data := struct {
+					D []byte
+				}{
+					D: ds[n],
+				}
+
+				if err := batch.Index(strconv.Itoa(n), data); err != nil {
+					panic(fmt.Sprintf("failed to index doc: %s", err.Error()))
+				}
+
+				if batch.Size() == i.batchSz {
+					if err := b.Batch(batch); err != nil {
+						panic(fmt.Sprintf("failed to index batch: %s", err.Error()))
+					}
+					batch = b.NewBatch()
+				}
+			}
+		}(s, docs[base:base+docsPerShard])
+		base = base + docsPerShard
+	}
+
+	wg.Wait()
+	return nil
+}
+
+// Count returns the total number of documents indexed.
+func (i *Indexer) Count() (uint64, error) {
+	return i.alias.DocCount()
+}
+
+func mapping() *bleve.IndexMapping {
+	// a generic reusable mapping for english text
+	standardJustIndexed := bleve.NewTextFieldMapping()
+	standardJustIndexed.Store = false
+	standardJustIndexed.IncludeInAll = false
+	standardJustIndexed.IncludeTermVectors = false
+	standardJustIndexed.Analyzer = "standard"
+
+	articleMapping := bleve.NewDocumentMapping()
+
+	// body
+	articleMapping.AddFieldMappingsAt("body", standardJustIndexed)
+
+	indexMapping := bleve.NewIndexMapping()
+	indexMapping.DefaultMapping = articleMapping
+	indexMapping.DefaultAnalyzer = "standard"
+	return indexMapping
+}
---
+++ cmd/bench/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/otoolep/bleve-bench"
+)
+
+var batchSize = flag.Int("batchSize", 100, "batch size for indexing")
+var nShards = flag.Int("shards", 1, "number of indexing shards")
+var maxprocs = flag.Int("maxprocs", 1, "GOMAXPROCS")
+var indexPath = flag.String("index", "indexes", "index storage path")
+var docsPath = flag.String("docs", "docs", "path to docs file")
+var csv = flag.Bool("csv", false, "summary CSV output")
+
+func main() {
+	flag.Parse()
+
+	runtime.GOMAXPROCS(*maxprocs)
+
+	// Remove any existing indexes.
+	if err := os.RemoveAll(*indexPath); err != nil {
+		fmt.Println("failed to remove %s.", *indexPath)
+		os.Exit(1)
+	}
+
+	// Attempt to open the file.
+	fmt.Printf("Opening docs file %s\n", *docsPath)
+	f, err := os.Open(*docsPath)
+	if err != nil {
+		fmt.Printf("failed to open docs file: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// Read the lines into memory.
+	docs := make([][]byte, 0, 100000)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		l := scanner.Text()
+		docs = append(docs, []byte(l))
+	}
+	fmt.Printf("%d documents read for indexing.\n", len(docs))
+
+	if len(docs)%(*nShards) != 0 {
+		fmt.Println("Document count must be evenly divisible by shard count")
+		os.Exit(1)
+	}
+
+	i := indexer.New(*indexPath, *nShards, *batchSize)
+	if err := i.Open(); err != nil {
+		fmt.Println("failed to open indexer:", err)
+		os.Exit(1)
+	}
+
+	startTime := time.Now()
+	if err := i.Index(docs); err != nil {
+		fmt.Println("failed to index documents:", err)
+		os.Exit(1)
+	}
+	duration := time.Now().Sub(startTime)
+
+	count, err := i.Count()
+	if err != nil {
+		fmt.Println("failed to determine total document count")
+		os.Exit(1)
+	}
+	rate := int(float64(count) / duration.Seconds())
+
+	fmt.Printf("Commencing indexing. GOMAXPROCS: %d, batch size: %d, shards: %d.\n",
+		runtime.GOMAXPROCS(-1), *batchSize, *nShards)
+
+	fmt.Println("Indexing operation took", duration)
+	fmt.Printf("%d documents indexed.\n", count)
+	fmt.Printf("Indexing rate: %d docs/sec.\n", rate)
+
+	if *csv {
+		fmt.Printf("csv,%d,%d,%d,%d,%d,%d\n", len(docs), count, runtime.GOMAXPROCS(-1), *batchSize, *nShards, rate)
+	}
+}
